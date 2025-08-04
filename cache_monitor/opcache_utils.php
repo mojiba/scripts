5ee9b35616925b447bfdbc966e5c84e47f77ff37
mojiba <mojiba@gmail.com>
2025-08-03 22:44:15 -0300

    Delete cache_monitor directory

--- cache_monitor/opcache_utils.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getOPcacheStatus() {
-    if (function_exists('opcache_get_status')) {
-        $status = @opcache_get_status(false);
-        return $status ?: [];
-    }
-    return [];
-}
